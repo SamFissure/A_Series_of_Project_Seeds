c07a52b09488f7a21a8bd93ffeb5e4bc0d816766
SamFissure <rpbaumeister@gmail.com>
2020-01-13 21:50:44 -0600

    Create rewardsevaluator.cpp
    Just barebones psuedocode for another calculation I do in my head that I want to outsource to a computer.

---
+++ rewardsevaluator.cpp
@@ -0,0 +1,5 @@
+//buncha include statements
+
+//PPD = Points/Price of item
+//VPP = Estimated Reward Value/Reward cost in points
+//Percent return = PPD*VPP (keep in mind, both are fractions, and it may be nice for them to stay that way.)
