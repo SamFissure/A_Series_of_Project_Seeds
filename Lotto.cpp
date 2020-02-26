ec4ede81283343f482d064faf31a0fffb765c2e9
SamFissure <rpbaumeister@gmail.com>
2020-02-25 23:09:33 -0600

    Create Lotto.cpp
    Added another project idea.

---
+++ Lotto.cpp
@@ -0,0 +1,9 @@
+/*A series of ideas for a "satisfying" lotto number picker
+(because humans don't like clustered numbers.)
+1. Ranges with Random
+2. Dynamic ranges with random
+3. Dynamic adaptive random
+4. Consider "intelligent exclusion" (this may be some work, and will definitely be outside scope.)
+
+This is all about the customer getting a "satisfying" lotto number that is
+at least PARTIALLY random.  Clusters are not satisfying, even though they occur naturally in true random numbers.
